7149a822dafc76772850c95f4e1945fd7e3ae65a
Tsypin Ilya <ilyaboy@list.ru>
2022-05-29 13:15:26 +0300

    Change type of Id and private set generate Id in ctor

--- KindergartenApp/Entities/BaseEntity.cs
+++ KindergartenApp/Entities/BaseEntity.cs
@@ -1,11 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace KindergartenApp.Models;
 
 public abstract class BaseEntity
 {
     [BsonId]
-    public string Id { get; set; }
+    public ObjectId Id { get; private set; }
 
     public DateTimeOffset Created { get; private set; }
 
@@ -13,6 +14,7 @@ public abstract class BaseEntity
     
     protected BaseEntity()
     {
+        Id = ObjectId.GenerateNewId();
         Created = DateTimeOffset.UtcNow;
     }
 }
